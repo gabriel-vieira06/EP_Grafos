2651fc05161e8532950cd1b4c0df6b7cd0102380
Gabriel Vieira <gabrielvieira.cavalcante@gmail.com>
2023-04-07 12:47:26 -0300

    receber arcos e custos do digrafo

--- ep1.c
+++ ep1.c
@@ -17,7 +17,6 @@ void algoritmoDijkstra(); //[WIP]
 
 // Funcoes auxiliares
 
-int** criaDigrafo(); //[WIP]
 
 
 int main()
@@ -27,16 +26,21 @@ int main()
 	
 	return 0;
 	
-} // Fim main
+}	// Fim main
 
 void recebeArquivo()
 {    
-	char nomeArquivo[50];
+	char nomeArquivo[50] = "digrafo.txt";
 	FILE *arquivo;
-	int n,m,s,t;
 	
+	int** arcos;
+	double* custos;
+	int n,m,s,t,i,j;
+	
+	/*
 	printf("\n\tDigite o nome e extensao do arquivo a ser lido: ");
 	scanf("%s%*c", nomeArquivo);
+	*/
 	
 	arquivo = fopen(nomeArquivo, "rt");
 	
@@ -48,9 +52,66 @@ void recebeArquivo()
 	
 	fscanf(arquivo,"%d %d %d %d", &n, &m, &s, &t);
 	
-	// TESTA SE OS VALORES FORAM RECEBIDOS CORRETAMENTE
-	printf("%d %d %d %d", n, m, s, t);
+	// Recebe os arcos e custos a partir do arquivo aberto
+	
+	arcos = malloc(sizeof (int*) * m);
+	custos = malloc(sizeof (double) * m);
+	
+	if (arcos == NULL || custos == NULL){
+		printf("\n\n\tMemória insuficiente.");
+		return;	// Falta de memoria.
+	}
+	for (i = 0; i < m; i++)
+	{
+		arcos[i] = malloc(sizeof(int) * 2);
+		if (arcos[i] == NULL)		// Falta de memoria.
+		{
+			for (j = 0; j < i; j++) free (arcos[j]);
+			free (arcos);
+			printf("\n\n\tMemória insuficiente.");
+			return;
+		}
+	}
+	
+	for(i = 0; i < m; i++)
+	{
+		for(j = 0; j <= 2; j++)
+		{
+			if(j != 2){
+				fscanf(arquivo,"%d", &arcos[i][j]);	// Recebe um vertice do arco
+			}else{
+				fscanf(arquivo,"%lf", &custos[i]);	// Recebe o custo do arco
+			}	
+	 	}
+	}
+	
+	algoritmoDijkstra(); //[WIP]
+	
+	/* Testa se os valores foram recebidos devidamente
+	for(i = 0; i < m; i++)
+	{
+		for(j = 0; j <= 2; j++)
+		{
+			printf("\n");
+			if(j != 2){
+				printf("%d ", arcos[i][j]);
+			}else{
+				printf("%.1lf ", custos[i]);
+			}	
+	 	}
+	}
+	*/
+	
+	for (i = 0; i < m; i++) free(arcos[i]);
+	free(arcos);
+	free(custos);
 	
 	fclose(arquivo);
 	
-}
+}	// Fim recebeArquivo
+
+void algoritmoDijkstra(){ //[WIP]
+	
+	printf("Implementar Dijkstra...");
+	
+}	// Fim algoritmoDijkstra
